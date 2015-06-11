cf7b72d1e3c7b7c6751ae4a08aa9d2bbb50353ca
Samar Panda <samar.panda84@gmail.com>
2015-06-11 11:10:25 +0530

    basic linkedlist

---
+++ javascripts/linkedlist.js
@@ -0,0 +1,56 @@
+function LinkedList(){
+	this._head = null;
+	this._length = 0;
+}
+
+LinkedList.prototype = {
+	add: function(data) {
+		var node = {
+			data: data,
+			next: null
+		},
+		current;
+		if(this._head === null){
+			this._head = data;
+		} else {
+			current = this._head;
+			while(current.next){
+				current = current.next;
+			}
+			current.next = node;
+		}
+		this._length++;
+	},
+	item: function(index) {
+		if(index > -1 && index < this._length){
+			var current = this._head,
+				i = 0;
+			while(i++ < index){
+				current = current.next;
+			}
+			return current.data;
+		} else {
+			return null;
+		}
+	},
+	remove: function(index) {
+		if(index > -1 && index < this._length){
+			var current = this._head,
+				previous,
+				i = 0;
+			if(index === 0){
+				this._head = current.next;
+			} else {
+				while(i++ < index){
+					previous = current;
+					current = current.next;
+				}
+				previous.next = current.next;
+			}
+			this._length--;
+			return current.data;
+		} else {
+			return null;
+		}
+	}
+};
